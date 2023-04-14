bd0bc0c2665f36622050cb6f6afe0ce5c2dfd49d
Muganwa <stevenmuganwa@live.com>
2023-04-14 10:59:20 +0300

    guess struct

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,30 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+        Guess { value }
+    }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
     loop {
@@ -11,10 +35,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         println!("You guessed: {guess}");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+        if (guess < 1) || (guess > 100) {
+            println!("Please input a number between 1 and 100");
+            continue;
+        }
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
