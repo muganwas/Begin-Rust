f42d65f95f75771d7e17282dcdf41dc250fb1a80
Muganwa <stevenmuganwa@live.com>
2023-04-18 10:33:44 +0300

    removing duplicates

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+    let largest = _largest(&number_list);
+    println!("The largest number is {}", largest);
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let largest = _largest(&number_list);
+    println!("The largest number is {}", largest);
+}
+
+fn _largest(list: &Vec<i32>) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
 }
