485eda92ad9f7a832f66460582ac12ba09d2260f
Muganwa <stevenmuganwa@live.com>
2023-04-19 15:34:05 +0300

    ordering different types separately

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,13 +1,23 @@
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let largest = _largest(&number_list);
-    println!("The largest number is {}", largest);
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let largest = _largest(&number_list);
+    let largest = _largest_i32(&number_list);
     println!("The largest number is {}", largest);
+    let char_list = vec!['y', 'm', 'a', 'q', 'z'];
+    let largest = _largest_charactor(&char_list);
+    println!("The largest charactor is {}", largest);
 }
 
-fn _largest(list: &Vec<i32>) -> i32 {
+fn _largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn _largest_charactor(list: &[char]) -> char {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
