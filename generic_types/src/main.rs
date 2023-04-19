fbafb0709279e4b49e950e40d9c76e0a14330d89
Muganwa <stevenmuganwa@live.com>
2023-04-19 15:59:59 +0300

    defining generic types for functions

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,25 +1,15 @@
 fn main() {
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let largest = _largest_i32(&number_list);
+    let largest = _largest(&number_list);
     println!("The largest number is {}", largest);
     let char_list = vec!['y', 'm', 'a', 'q', 'z'];
-    let largest = _largest_charactor(&char_list);
+    let largest = _largest(&char_list);
     println!("The largest charactor is {}", largest);
 }
 
-fn _largest_i32(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-    largest
-}
-
-fn _largest_charactor(list: &[char]) -> char {
-    let mut largest = list[0];
-    for &item in list.iter() {
+fn _largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
         if item > largest {
             largest = item;
         }
