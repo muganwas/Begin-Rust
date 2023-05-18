96bb51c982331bb1497e0a15fda5c98addf697f1
Muganwa <stevenmuganwa@live.com>
2023-05-18 18:02:54 +0300

    workspaces

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add_one(num: i32) -> i32 {
+    num + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(2);
+        assert_eq!(result, 3);
+    }
+}
---
+++ add/adder/src/main.rs
@@ -0,0 +1,6 @@
+use add_one::add_one;
+
+fn main() {
+    let tree = add_one(2);
+    println!("when you add one to two: {}", tree);
+}
